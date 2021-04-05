2420b62b0ee53ba52475f960df996bac4b1b9bee
Den107 <deniskrv@gmail.com>
2021-04-05 19:10:59 +0300

    app done

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quo
+      voluptates, laboriosam amet dolorem voluptas eius? Dolore iusto, dolorem
+      ullam maxime placeat repellat quisquam, nemo doloremque magni perspiciatis
+      esse illum.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
